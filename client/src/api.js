fdb983894d0c654c46fcc77c612007b0fad82555
IMAYAVAN <imayavanmanivel004@gmail.com>
2026-04-08 19:45:47 +0530

    Update API base URL to production endpoint

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://student-cognitive-skill-assessment-tool.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
